58c72a61b7711d7b216c1c021b785937438f8868
Okkan Chávez <ochavezg2@miumg.edu.gt>
2022-04-30 10:40:16 -0600

    Creé la clase padre llamada FigurasGemotricas y en la función principal main creé un switch para que el usuario pueda eligir la figura geometrica que desee

---
+++ FigurasGeometricas.java
@@ -0,0 +1,12 @@
+//Clase padre
+package segundoparcial;
+
+public class FigurasGeometricas {
+    
+    protected float pi;
+    
+    FigurasGeometricas(){
+    this.pi=(float) 3.14;
+    }
+    
+}
---
+++ SegundoParcial.java
@@ -0,0 +1,40 @@
+
+package segundoparcial;
+
+import java.util.Scanner;
+
+public class SegundoParcial {
+
+    public static void main(String[] args) {
+      
+        int opcion;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("    Bienvenido al Programa de Figuras Geométricas   ");
+        System.out.println("    1. Círculo");
+        System.out.println("    2. Cuadrado");
+        System.out.println("    3. Tríangulo");
+        System.out.println("    4. Cilindro");
+        System.out.println("    5. Prisma");
+        System.out.println("    6. Esfera");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Ingresa el número de la opción que quieres: "); opcion = entrada.nextInt();
+        
+        switch(opcion){
+        
+        case 1:
+            
+            
+        break;
+        
+        }
+
+    }
+    
+}
+        /*
+        case 1:
+        break;
+      */ 
